69ad7e72f6d3aa6c4ea4d84d2c49f2d997fcd9d1
Prajitkhawshi <prajit@nimapinfotech.com>
2023-02-15 18:58:40 +0530

    program

--- src/com/logic/array/MoveAllZero.java
+++ src/com/logic/array/MoveAllZero.java
@@ -1,20 +1,27 @@
 package com.logic.array;
 
+import java.util.ArrayList;
+
 public class MoveAllZero {
 	public static void main(String[] args) {
 
 		int arr[] = { 1, 2, 0, 4, 3, 0, 5, 0 };
+		int sss[] = new int[arr.length];
 		int count = 0;
+		ArrayList<Integer> arrayList = new ArrayList<>();
 		for (int i = 0; i < arr.length; i++) {
-
 			if (arr[i] != 0) {
-				System.out.println(arr[i]);
+				arrayList.add(arr[i]);
 			}
-			if (arr[i] == 0) {
-				count++;
 
-			}
 		}
+		for (int j = 0; j < arr.length; j++) {
+			if (arr[j] == 0) {
+				arrayList.add(arr[j]);
+			}
+
+		}
+		System.out.println(arrayList);
 	}
 
 }
